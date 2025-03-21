74014e57583790b6b4e81682c5fa00b87be50623
brob23br <152234119+brob23br@users.noreply.github.com>
2025-03-20 20:23:45 -0400

    Add files via upload

--- CS50 Week 1 Math Interpreter PS1.py
+++ CS50 Week 1 Math Interpreter PS1.py
@@ -8,12 +8,25 @@ z is an integer
 For instance, if the user inputs 1 + 1, your program should output 2.0. Assume that, if y is /, then z will not be 0.
 
 Note that, just as python itself is an interpreter for Python, so will your interpreter.py be an interpreter for math!'''
+from importlib import invalidate_caches
 
-query = input("Expression: ")
+query = input("Expression: ").strip()
 
 x, y, z = query.split(" ")
 
+x = int(x)
+z = int(z)
 
+if y == "+":
+    print(float(x + z))
+elif y == "-":
+    print(float(x - z))
+elif y == "*":
+    print(float(x * z))
+elif y == "/":
+    print(float(x / z))
+else:
+    print("Invalid")
 
 
 
