d7a95c176e5dfcd8cbeda4f4c2c58ec1ce12b66a
brob23br <152234119+brob23br@users.noreply.github.com>
2025-03-20 20:36:04 -0400

    Update CS50 Week 1 Math Interpreter PS1.py

--- CS50 Week 1 Math Interpreter PS1.py
+++ CS50 Week 1 Math Interpreter PS1.py
@@ -8,7 +8,6 @@ z is an integer
 For instance, if the user inputs 1 + 1, your program should output 2.0. Assume that, if y is /, then z will not be 0.
 
 Note that, just as python itself is an interpreter for Python, so will your interpreter.py be an interpreter for math!'''
-from importlib import invalidate_caches
 
 query = input("Expression: ").strip()
 
