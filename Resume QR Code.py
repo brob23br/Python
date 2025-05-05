1aa778884a7136ccdb0fa80681875fabb56c6263
brob23br <152234119+brob23br@users.noreply.github.com>
2025-05-05 14:54:16 -0400

    Update Resume QR Code.py

--- Resume QR Code.py
+++ Resume QR Code.py
@@ -1,7 +1,5 @@
 import qrcode
 
-url = "https://1drv.ms/b/c/b9b1d2e362f939e0/EUsO8z7BEq9CpzEs7ZKEiZgBdINzU4Td9PSl2FDQWNwewQ?e=lLGavZ"
-
+url = "https://"
 qr = qrcode.make(url)
-
 qr.save("resume_qr 5-5-25.png")
